b81a726419e70fdb41e52ea3831e239e9acf6165
Jewell <jhuffman815@gmail.com>
2020-03-31 23:06:27 -0400

    Initial Commit/Setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+      <div>
+          <h1>Title</h1>
+          <textarea value="some stuff" />
+          <h4>Time Remaining: 0</h4>
+          <button>start game</button>
+          <h1>Word Count</h1>
+      </div>
+  )
+}
+
+
+export default App;
