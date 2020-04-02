898af031728e424bc99e0cee57c5fb79970654d6
Jewell <jhuffman815@gmail.com>
2020-04-01 23:19:59 -0400

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './App.css';
 
 function App() {
+  const [text, setText] = useState("")
+
+  function handleChange(e) {
+    const{value} = e.target
+    setText(value)
+  }
+
+  console.log(text)
   return (
       <div>
           <h1>Title</h1>
-          <textarea value="some stuff" />
+          <textarea 
+            onChange={handleChange} 
+            value={text}/>
           <h4>Time Remaining: 0</h4>
           <button>start game</button>
           <h1>Word Count</h1>
